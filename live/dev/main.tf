0d73036a649129accb44ed719ad93619c20ab7b4
jvjfranca <jvjfranca@gmail.com>
2022-04-12 07:15:43 -0300

    WebServer Variable TCP port

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "webserver" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webserver.id]
   user_data              = <<-EOF
-  #!/bin/bash
-  echo ""Hello, World" > index.html
-  nohup busybox httpd -f -p 8080 &
-  EOF
+                          #!/bin/bash
+                          echo "Hello, World" > index.html
+                          nohup busybox httpd -f -p ${var.webserver_port} &
+                          EOF
   tags = {
     Name       = "Webserver"
     Automation = "Terraform"
@@ -16,8 +16,8 @@ resource "aws_instance" "webserver" {
 resource "aws_security_group" "webserver" {
   name = "sg_webserver"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.webserver_port
+    to_port     = var.webserver_port
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow HTTP Anywere"
     protocol    = "tcp"
