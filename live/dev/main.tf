ecf7f394c16a6263305e4c13f6b7e71e40d38338
jvjfranca <jvjfranca@gmail.com>
2022-04-12 06:51:17 -0300

    Simple WebServer

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webserver" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  ami                    = "ami-04505e74c0741db8d" # Ubuntu AMI
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webserver.id]
-  user_data     = <<-EOF
+  user_data              = <<-EOF
   #!/bin/bash
   echo ""Hello, World" > index.html
   nohup busybox httpd -f -p 8080 &
@@ -15,11 +15,11 @@ resource "aws_instance" "webserver" {
 
 resource "aws_security_group" "webserver" {
   name = "sg-webserver"
-  ingress = [ {
-    from_port = 80
-    to_port = 80
-    cidr_blocks = [ "0.0.0.0/0" ]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
     description = "Allow HTTP Anywere"
-    protocol = "tcp"
-  } ]
+    protocol    = "tcp"
+  }
 }
---
+++ modules/web-server/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "webserver" {
+  ami                    = "ami-04505e74c0741db8d" # Ubuntu AMI
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+  user_data              = <<-EOF
+  #!/bin/bash
+  echo ""Hello, World" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
+  tags = {
+    Name       = "Webserver"
+    Automation = "Terraform"
+  }
+}
+
+resource "aws_security_group" "webserver" {
+  name = "sg-webserver"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP Anywere"
+    protocol    = "tcp"
+  }
+}
